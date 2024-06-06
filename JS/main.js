10a9eeb86360ac0534a1772a7da1623c4dd4b489
Gusttavobarcell <146050749+Gusttavobarcell@users.noreply.github.com>
2024-06-06 12:19:26 -0300

    att

--- JS/main.js
+++ JS/main.js
@@ -36,53 +36,6 @@ function splitTextIntoSpans(target) {
 
 splitTextIntoSpans('.animacaotext')
 // seleção de tema para o site modo claro e modo escuro
-// seleciona informação da lista para mudar tema do site 
-// const lightModeButton = document.getElementById('lightMode');
-// const darkModeButton = document.getElementById('darkMode');
-// const container = document.getElementById('dark');
-// const themeCheckbox = document.getElementById('themeCheckbox');
-// const bgImovelDark = document.getElementById('bg_imovel_dark');
-// const bgImovelDark2 = document.getElementById('bg_imovel_dark2');
-// const mensagemalerta = document.getElementById('mensagemalerta');
-
-// Função para definir o tema com um atraso
-// function setThemeWithDelay(theme) {
-//     setTimeout(function () {
-//         setTheme(theme);
-//     }, 550);  Delay de 500 milissegundos (0.5 segundo)
-// }
-
-// Adiciona um event listener para o evento change do checkbox
-// themeCheckbox.addEventListener('change', function () {
-//     Verifica se o checkbox está marcado
-//     if (this.checked) {
-//         setThemeWithDelay('dark');
-//     } else {
-//         setThemeWithDelay('light');
-//     }
-// });
-
-// function setTheme(theme) {
-//     Adiciona a classe correspondente ao tema
-//     if (theme === 'dark') {
-//         container.classList.remove('light');
-//         container.classList.add('dark');
-//         bgImovelDark.style.backgroundColor = '#080e10';
-//         bgImovelDark2.style.backgroundColor = '#080e10';
-//         mensagemalerta.style.backgroundColor = '#080e10';
-
-//     } else {
-//         container.classList.remove('dark');
-//         container.classList.add('light');
-//         bgImovelDark.style.backgroundColor = '#e6e6e6';
-//         bgImovelDark2.style.backgroundColor = '#e6e6e6';
-//         mensagemalerta.style.backgroundColor = '#e6e6e6';
-
-//     }
-//     Salva a preferência do usuário no armazenamento local (localStorage)
-//     localStorage.setItem('theme', theme);
-// }
-
 
 document.addEventListener('DOMContentLoaded', function () {
     const themeCheckbox = document.getElementById('themeCheckbox');
