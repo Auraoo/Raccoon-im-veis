2af307fc9409a71eb5bda5d809bc03bd925fed57
Gusttavobarcell <146050749+Gusttavobarcell@users.noreply.github.com>
2024-06-13 12:08:54 -0300

    att

--- ADM_CRUD/corretores_mostrar.php
+++ ADM_CRUD/corretores_mostrar.php
@@ -23,8 +23,8 @@ if ($result->num_rows > 0) {
         <div class="container p-2">
         <div class="row info-container fundo_background_dark corretor_background_div">
              
-            <div class="col-md-4 p-0" >
-                <img src="Raccon-im-veis/'.$row['foto_corretor'].'" alt="Imagem Exemplo"  style="height: 190px">
+            <div class="col-md-4 p-0" style="z-index: 30!important;" >
+                <img src="Raccon-im-veis/'.$row['foto_corretor'].'" alt="Imagem Exemplo"  style="height: 190px;">
             </div>
             <div class="col-md-8 p-2 info fundo_background_dark corretor_background_info">
 
